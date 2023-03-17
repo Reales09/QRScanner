3d297511acf14c7191a2bb4cb6b2a33d4368d003
Reales <rclarence123@gmail.com>
2023-03-17 15:39:21 -0500

    Se diseña home Screen se agrega bottom navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner/widgets/custom_navigator.dart';
+import 'package:qr_scanner/widgets/scan_button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +8,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('historial'),
+        elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: Icon(Icons.delete_forever_outlined))
+        ],
+      ),
       body: Center(
         child: Text('Home page'),
       ),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
