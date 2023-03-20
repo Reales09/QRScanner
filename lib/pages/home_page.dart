0fbd63ccb455349780c2dfa823ef27632b1379d2
Reales <rclarence123@gmail.com>
2023-03-20 18:35:58 -0500

    Se agrega index para navegacion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner/pages/direcciones_page.dart';
+import 'package:qr_scanner/pages/mapas_page.dart';
 import 'package:qr_scanner/widgets/custom_navigator.dart';
 import 'package:qr_scanner/widgets/scan_button.dart';
 
@@ -16,12 +18,31 @@ class HomePage extends StatelessWidget {
               onPressed: () {}, icon: Icon(Icons.delete_forever_outlined))
         ],
       ),
-      body: Center(
-        child: Text('Home page'),
-      ),
+      body: _HomePageBody(),
       bottomNavigationBar: CustomNavigationBar(),
       floatingActionButton: ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  const _HomePageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Cambiar para mostrar la página respectiva
+    final currentIndex = 1;
+
+    switch (currentIndex) {
+      case 0:
+        return MapasPage();
+
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
   }
 }
